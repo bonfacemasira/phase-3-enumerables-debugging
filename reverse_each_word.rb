146a475813c3371c9a49ff21a294d5fa67f57cc5
Bonface Masira <bonfacemasira@gmail.com>
2022-08-20 02:19:47 +0300

    code along

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,36 @@
 # Write your code here
+require 'pry'
+
+sentence = "Hello there, and how are you?"
+def reverse_each_word (sentence)
+    words = sentence.split
+    reversed_words = []
+
+    words.each { |word| reversed_words << word.reverse }
+
+    reversed_words.join(" ")
+end
+
+def reverse_each_word(sentence)
+    words = sentence.split
+  
+    reversed_words = words.map(&:reverse)
+  
+    reversed_words.join(" ")
+  end
+
+def reverse_each_word(sentence)
+    words = sentence.split
+  
+    reversed_words = words.map do |word|
+      word.reverse
+    end
+  
+    reversed_words.join(" ")
+  end
+
+def reverse_each_word(sentence)
+    sentence.split.map(&:reverse).join(" ")
+  end
+
+puts reverse_each_word("Hello there, and how are you?")
